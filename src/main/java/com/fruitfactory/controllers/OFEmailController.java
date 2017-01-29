b6ce3e0e7a159fee459821bb4fe9ac56a8d6d2d4
Yariki <iyariki.ya@gmail.com>
2017-01-29 18:21:40 +0200

    Initialize commit

---
+++ src/main/java/com/fruitfactory/controllers/OFEmailController.java
@@ -0,0 +1,16 @@
+package com.fruitfactory.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Yariki on 1/29/2017.
+ */
+@Controller
+@RequestMapping(value = "/email")
+public class OFEmailController {
+
+
+
+
+}
